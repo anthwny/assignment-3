a36fa6dd0523b81c6c2c3640895f33a6ccc0f107
anthony <anthony75368@gmail.com>
2024-10-31 23:06:22 -0400

    Formatted code + Added CSS grid

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-document.getElementById("radian-or-degree-button").addEventListener("click", function(){
-    let button = document.getElementById("radian-or-degree-button")
-    if (button.innerHTML == "Degrees") {
-        button.innerHTML = "Radians";
-    }
-    else if (button.innerHTML == "Radians") {
-        button.innerHTML = "Degrees";
-    }
-})
-
 function cosineLaw(a, b, C) {
-    let radianOrDegree = document.getElementById("radian-or-degree-button").innerHTML
+    let radianOrDegree = document.getElementById("radian-or-degree-button").innerHTML;
+    let cosineValue = 0;
     if (radianOrDegree == 'Radians') {
         cosineValue = Math.cos(C);
-    }
-    else {
+    } else {
         cosineValue = Math.cos((C * Math.PI) / 180);
     }
-    return Math.sqrt(a * a + b * b - 2 * a * b * cosineValue)
+    return Math.sqrt(a * a + b * b - 2 * a * b * cosineValue);
 }
 
 function asymptoteFinder(m, n) {
-    const asymptoteType = ["Linear", "Quadratic", "Cubic", "Quartic", "Quintic", "Sextic", "Septic", 
-    "Octic", "Nonic", "Decic"];
+    const asymptoteType = ["Linear", "Quadratic", "Cubic", "Quartic", "Quintic", "Sextic", "Septic", "Octic", "Nonic", "Decic"];
     if (m == n) {
         return "The asymptote is horizontal";
-    }
-    else if (n > m) {
+    } else if (n > m) {
         return "The asymptote is the x-axis";
-    }
-    else if (m > n) {
-        return "The asymptote is " + asymptoteType[m-n-1];
-    }
-    else {
+    } else if (m > n) {
+        return "The asymptote is " + asymptoteType[m - n - 1];
+    } else {
         return "Error";
     }
 }
@@ -39,28 +25,36 @@ function asymptoteFinder(m, n) {
 function piApproximation(n) {
     let approximation = 0;
     for (let i = 0; i < n; i++) {
-        approximation += 4*(Math.pow((-1), i) / (2*i + 1))
+        approximation += 4 * (Math.pow((-1), i) / (2 * i + 1));
     }
     return approximation;
 }
 
 /* Event listeners in order to communicate with HTML */
 
-document.getElementById("cos-button").addEventListener("click", function(){ 
-    let cosA = parseInt(document.getElementById("input-a").value);
-    let cosB = parseInt(document.getElementById("input-b").value);
-    let cosC = parseInt(document.getElementById("input-C").value);
-    document.getElementById("cos-result").value = cosineLaw(cosA, cosB, cosC);
-    });
+document.getElementById("radian-or-degree-button").addEventListener("click", function () {
+    let button = document.getElementById("radian-or-degree-button");
+    if (button.innerHTML == "Degrees") {
+        button.innerHTML = "Radians";
+    } else if (button.innerHTML == "Radians") {
+        button.innerHTML = "Degrees";
+    }
+})
 
-document.getElementById("asymp-button").addEventListener("click", function(){ 
-    let asympM = parseInt(document.getElementById("input-m").value);
-    let asympN = parseInt(document.getElementById("input-asymp-n").value);
-    document.getElementById("asymp-result").value = asymptoteFinder(asympM, asympN);
-    });
-    
-document.getElementById("pi-button").addEventListener("click", function(){ 
-    let piN = parseInt(document.getElementById("input-pi-n").value);
-    document.getElementById("pi-result").value = piApproximation(piN);
+document.getElementById("cos-button").addEventListener("click", function () {
+    let cosA = document.getElementById("cos-a").value;
+    let cosB = document.getElementById("cos-b").value;
+    let cosC = document.getElementById("cos-C").value;
+    document.getElementById("cos-result").value = cosineLaw(cosA, cosB, cosC);
 });
 
+document.getElementById("asymp-button").addEventListener("click", function () {
+    let asympM = parseInt(document.getElementById("asymp-m").value);
+    let asympN = parseInt(document.getElementById("asymp-n").value);
+    document.getElementById("asymp-result").value = asymptoteFinder(asympM, asympN);
+});
+
+document.getElementById("pi-button").addEventListener("click", function () {
+    let piN = document.getElementById("pi-n").value;
+    document.getElementById("pi-result").value = piApproximation(piN);
+});
